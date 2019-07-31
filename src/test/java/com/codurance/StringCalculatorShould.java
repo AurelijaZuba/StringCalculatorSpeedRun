11e7e9366d753360c2487be3ed5981e2d3af5740
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 13:13:57 +0100

    Refactoring - implement parameterized tests

--- src/test/java/com/codurance/StringCalculatorShould.java
+++ src/test/java/com/codurance/StringCalculatorShould.java
@@ -1,40 +1,23 @@
 package com.codurance;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorShould {
-
-    @Test
-    void return_zero_when_string_is_empty() {
+    @ParameterizedTest
+    @CsvSource({
+            "'', 0",
+            "'4', 4",
+            "'1, 2', 3"
+    })
+    void add_numbers(String input, int expected) {
         StringCalculator stringCalculator = new StringCalculator();
 
-        int actual = stringCalculator.add("");
+        int actual = stringCalculator.add(input);
 
-        int expected = 0;
         assertThat(actual).isEqualTo(expected);
     }
-
-    @Test
-    void return_4_when_string_is_only_4() {
-        StringCalculator stringCalculator = new StringCalculator();
-
-        int actual = stringCalculator.add("4");
-
-        int expected = 4;
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    void return_3_when_string_contains_one_and_two() {
-        StringCalculator stringCalculator = new StringCalculator();
-
-        int actual = stringCalculator.add("1, 2");
-
-        int expected = 3;
-        assertThat(actual).isEqualTo(expected);
-    }
-
-
 }
