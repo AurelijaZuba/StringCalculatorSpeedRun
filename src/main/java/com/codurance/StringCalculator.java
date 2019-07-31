3a6de5dc9cdc729ebeb830024f3103e0f3ce1092
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 13:41:38 +0100

    Refactoring - implement solution using for-loop

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -5,26 +5,17 @@ public class StringCalculator {
         if(numbers.equals(""))
             return 0;
 
-        int number1 = 0;
-        int number2 = 0;
+        int result = 0;
+
 
         final String[] splitNumbers = numbers
                 .replace(" ", "")
                 .split(",");
 
-        if(splitNumbers.length >= 1){
-           number1 = Integer.valueOf(splitNumbers[0]);
+        for (String value : splitNumbers) {
+            result += Integer.valueOf(value);
         }
 
-        if(splitNumbers.length >= 2){
-            number2 = Integer.valueOf(splitNumbers[1]);
-        }
-
-        if(splitNumbers.length >= 3) {
-            final Integer number3 = Integer.valueOf(splitNumbers[2]);
-            return number1 + number2 + number3;
-        }
-
-        return number1 + number2;
+        return  result;
     }
 }
