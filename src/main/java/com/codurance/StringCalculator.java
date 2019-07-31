842d4914de9e7e773f47abcafc7a9f9e43e13e08
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 13:36:58 +0100

    Passing test for multiple numbers "1, 2, 3"

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -7,6 +7,7 @@ public class StringCalculator {
 
         int number1 = 0;
         int number2 = 0;
+
         final String[] splitNumbers = numbers
                 .replace(" ", "")
                 .split(",");
@@ -14,8 +15,14 @@ public class StringCalculator {
         if(splitNumbers.length >= 1){
            number1 = Integer.valueOf(splitNumbers[0]);
         }
+
         if(splitNumbers.length >= 2){
             number2 = Integer.valueOf(splitNumbers[1]);
+        }
+
+        if(splitNumbers.length >= 3) {
+            final Integer number3 = Integer.valueOf(splitNumbers[2]);
+            return number1 + number2 + number3;
         }
 
         return number1 + number2;
