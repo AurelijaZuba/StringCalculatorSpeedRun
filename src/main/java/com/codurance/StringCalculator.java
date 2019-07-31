e8cc5379db03c380567536bf24102329d724e4a7
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-07-31 14:46:32 +0100

    Passes test for custom operator

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -7,16 +7,36 @@ public class StringCalculator {
 
         int result = 0;
 
+        String separator = null;
+
+        separator = extractCustomSeparator(numbers);
+
+        if(separator != null)
+            numbers = getCustomSeparatedValues(numbers);
+        else
+            separator = "[\\n|,]";
+
         final String[] splitNumbers = numbers
                 .replace(" ", "")
-                .split("[\\n|,]");
-
-        System.out.println(splitNumbers[0]);
+                .split(separator);
 
         for (String value : splitNumbers) {
             result += Integer.valueOf(value);
         }
 
         return  result;
+    }
+
+    private String getCustomSeparatedValues(String numbers) {
+        StringBuilder sb = new StringBuilder(numbers);
+        sb.delete(0, 5);
+        return sb.toString();
+    }
+
+    private String extractCustomSeparator(String numbers) {
+        if(!numbers.startsWith("//"))
+            return null;
+        StringBuffer sb = new StringBuffer(numbers);
+        return sb.substring(2,3);
     }
 }
