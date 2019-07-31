9eec45f16d5f6e1838c203f51d06b83e2577baf9
AurelijaZuba <auryte00@hotmail.com>
2019-07-31 11:57:16 +0100

    Failing test empty string

--- src/test/java/com/codurance/StringCalculatorShould.java
+++ src/test/java/com/codurance/StringCalculatorShould.java
@@ -1,4 +1,18 @@
 package com.codurance;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringCalculatorShould {
+
+    @Test
+    void return_zero_when_string_is_empty() {
+        StringCalculator stringCalculator = new StringCalculator();
+
+        int actual = stringCalculator.add("");
+
+        int expected = 0;
+        assertThat(actual).isEqualTo(expected);
+    }
 }
