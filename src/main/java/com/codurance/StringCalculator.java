544e9f5ff58731c55c1e6cdac7bf5aa40475f2af
AurelijaZuba <auryte00@hotmail.com>
2019-07-31 13:27:53 +0100

    Passing test for return 5 when input "2, 3"

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -2,12 +2,20 @@ package com.codurance;
 
 public class StringCalculator {
     public int add(String numbers) {
-        if(numbers.equals("2, 3"))
-            return 5;
-        if(numbers.equals("1, 2"))
-            return 3;
-        if(numbers.equals("4"))
-            return 4;
-        return 0;
+        int number1 = 0;
+        int number2 = 0;
+        final String[] splitNumbers = numbers
+                .replace(" ", "")
+                .split(",");
+
+        if(splitNumbers.length == 1 && !splitNumbers[0].equals("")){
+           number1 = Integer.valueOf(splitNumbers[0]);
+        }
+        if(splitNumbers.length == 2 && !splitNumbers[0].equals("")){
+            number1 = Integer.valueOf(splitNumbers[0]);
+            number2 = Integer.valueOf(splitNumbers[1]);
+        }
+
+        return number1 + number2;
     }
 }
